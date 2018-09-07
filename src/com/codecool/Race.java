b019c20ffa4ed3c184767b6c447c5fcaf572d5b5
dewsy <dewsyy@gmail.com>
2018-09-07 16:52:59 +0200

    Car refactored, motor started

--- src/com/codecool/Race.java
+++ src/com/codecool/Race.java
@@ -7,52 +7,51 @@ import java.util.Random;
 public class Race {
 
 
-
     interface Vehicle {
 
         Random rand = new Random();
         String name;
         int distanceTraveled;
         int speed;
-        public void moveForAnHour()
+        public void moveForAnHour();
 
 
     }
 
     public static void main(String[] args) {
-	// write your code here
+
+        Random rand = new Random();
         createVehicles() // creates 10 cars, 10 trucks and 10 motorcycles.
         simulateRace() // simulates the race by
         // - calling moveForAnHour() on every vehicle 50 times
         // - setting whether its raining
-        printRaceResults() // prints each vehicle's name, distance traveled ant type.
+        printRaceResults() // prints each vehicle's name, distance traveled and type.
         boolean isThereABrokenTruck()//
 
+        boolean isRaining; // is it raining currently.
 
-        static setRaining() {} // 30% chance of rain.
+        static void setRaining() {
+            int rainOMeter = rand.nextInt(9);
+            if (rainOMeter <= 6) {
+               isRaining = false;
+           } else {
+                isRaining =true;
+            }
+        }
 
-        static boolean isRaining() // is it raining currently.
 
         class Car implements Vehicle{
 
-            String[] carNames = {"Chase", "Reflect", "Freedom", "Origin", "Stardust", "Flux",
+            private String[] carNames = {"Chase", "Reflect", "Freedom", "Origin", "Stardust", "Flux",
                     "Prophecy", "Inception", "Rune", "Formula"};
+            private int speed;
+            public String name;
+            public int distanceTraveled;
+
 
             private String pickName() {
                 return carNames[rand.nextInt(9)];
             }
-
-            private String createName() {
-                String firstName = pickName();
-                String lastName = pickName();
-                return String.format("%s %s", firstName, lastName );
-            }
-
-            private int speed;
-
-            private String name;
-
-            private int distanceTraveled;
 
             public void moveForAnHour() {
                 if (isThereABrokenTruck) {
@@ -64,18 +63,23 @@ public class Race {
 
             Car() {
                 this.speed = rand.nextInt(30) + 80;
-                this.name = createName();
+                this.name = String.format("%s %s", pickName(), pickName());
             }
 
 
             //!!!!! If there is a broken down Truck on the track, then limit the max speed of vehicles to 75 km/h.
         }
 
-        Motorcycle // speed is 100km/h. If it rains, travels with 5-50km/h slower (randomly).
-        static nameNumber // The number of the instance created. Used for its name.
-        name // Are called "Motorcycle 1", "Motorcycle 2", "Motorcycle 3",... Unique.
-        distanceTraveled
-        moveForAnHour(Race race)
+        class Motorcycle implements Vehicle { // speed is 100km/h. If it rains, travels with 5-50km/h slower (randomly).
+
+            int speed = 100;
+            static nameNumber // The number of the instance created. Used for its name.
+                    name // Are called "Motorcycle 1", "Motorcycle 2", "Motorcycle 3",... Unique.
+
+            distanceTraveled
+            public void moveForAnHour() {
+            }
+        }
 
         Truck // speed: 100km/h. 5% chance of breaking down for 2 hours.
         // Truck drivers are boring. They call all their trucks a random number between 0 and 1000.
